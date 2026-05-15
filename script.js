71a6cd2bbe55315ec025c1dd796bbb24086b75ca
dancoder-engineer <japaneseforgaming@gmail.com>
2026-05-15 15:54:28 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const fromBox = document.querySelector("#from")
+const toBox = document.querySelector("#to")
+const lines = document.querySelector("#linesNo")
+
+const assign = () => {
+    const noOfLines = parseInt(lines.value)
+    const names = fromBox.value.split("\n")
+    eachPerson = Math.floor(noOfLines / names.length)
+    
+    let assignments = ""
+    for(let i = 0; i < names.length;i++){
+        let start = (i * eachPerson) + 1
+        let finish = i === names.length - 1 ? noOfLines : start + eachPerson - 1
+        assignments += names[i] + ": lines " + start + " - " + finish + "\n"
+    }
+    toBox.value = assignments
+}
