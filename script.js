133e03e837ed361f48737ba2fd9e51caa0bf5e30
dancoder-engineer <japaneseforgaming@gmail.com>
2026-05-15 20:01:45 -0400

    Starts at line 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const assign = () => {
     
     let assignments = ""
     for(let i = 0; i < names.length;i++){
-        let start = (i * eachPerson) + 1
+        let start = i === 0  ? 2 : (i * eachPerson) + 1
         let finish = i === names.length - 1 ? noOfLines : start + eachPerson - 1
         assignments += names[i] + ": lines " + start + " - " + finish + "\n"
     }
